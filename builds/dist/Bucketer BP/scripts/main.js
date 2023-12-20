cfd1883017c1aa94bb505b15f4034a4181fdfaaf
Joe P Shoulak <joe@squarecompass.io>
2023-12-19 21:55:32 -0600

    switched to regolith

--- builds/dist/Bucketer BP/scripts/main.js
+++
@@ -1,6 +0,0 @@
-import { system } from '@minecraft/server';
-import { bucket } from "./bucketer.js";
-
-system.afterEvents.scriptEventReceive.subscribe(({ id, sourceBlock }) => {
-    if (id === "fibdev:bucketer_update") bucket(sourceBlock);
-});
---
+++ makefile
@@ -0,0 +1,3 @@
+Bucketer.mcaddon:
+	mkdir ./packs/data;
+	regolith apply-filter export
